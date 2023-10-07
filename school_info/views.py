e27f4550daa498fe9342c3a02cb19ae0338b73be
MaxShulha94 <maxshulga94@gmail.com>
2023-10-07 15:26:21 +0300

    add form and views.py changes commit

--- school_info/views.py
+++ school_info/views.py
@@ -2,19 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 from .models import Teacher, Group
+from .forms import TeacherForm, GroupForm
 
 
 def teacher_form(request):
     if request.method == "GET":
-        return render(request, "teacher_form.html")
-    print("Got from data", request.POST)
-    t = Teacher.objects.create(
-        first_name=request.POST["first_name"],
-        last_name=request.POST["last_name"],
-        birth_date=request.POST["birth_date"],
-    )
-    print("Teacher was created", t)
-    return render(request, "teacher_form.html")
+        form = TeacherForm()
+        return render(request, "teacher_form.html", {"form": form})
+    form = TeacherForm(request.POST)
+    if form.is_valid():
+        t = Teacher.objects.create(
+            first_name=request.POST["first_name"],
+            last_name=request.POST["last_name"],
+            birth_date=request.POST["birth_date"],
+        )
+        print("Teacher was created", t)
+    return render(request, "teacher_form.html", {"form": form})
 
 
 def print_teachers(request):
@@ -33,23 +36,25 @@ def print_teachers(request):
 
 def group_form(request):
     if request.method == "GET":
-        return render(request, "group_form.html")
-    print("Got from data", request.POST)
-    group_name = request.POST.get("group_name")
-    teacher_first_name = request.POST.get("teacher_first_name")
-    teacher_last_name = request.POST.get("teacher_last_name")
-    teacher = Teacher.objects.filter(
-        first_name=teacher_first_name, last_name=teacher_last_name
-    ).first()
-    if teacher:
-        group = Group.objects.create(
-            group_name=group_name,
-            teacher_name=teacher,
+        form = TeacherForm()
+        return render(request, "group_form.html", {"form": form})
+    form = GroupForm(request.POST)
+    if form.is_valid():
+        g = Group.objects.create(
+            group_name=request.POST["group_name"],
+            teacher_first_name=request.POST["teacher_first_name"],
+            teacher_last_name=request.POST["teacher_last_name"]
+
         )
-        print("Group was created", group)
-        return render(request, "group_form.html")
-    else:
-        print("Teacher not found")
+        # group_name = request.POST.get("group_name")
+        # teacher_first_name = request.POST.get("teacher_first_name")
+        # teacher_last_name = request.POST.get("teacher_last_name")
+        # teacher = Teacher.objects.filter(
+        #     first_name=teacher_first_name, last_name=teacher_last_name
+        # ).first()
+
+    return render(request, "group_form.html", {"form": form})
+
 
 
 def print_groups(request):
